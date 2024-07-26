6de26cbaaf97687b92ab9fe573e371821a1d8725
obada kraishan <obada.kraishan@gmail.com>
2024-07-26 03:54:15 -0500

    Tabs filter

--- lib/presentation/screens/products/products_screen.dart
+++ lib/presentation/screens/products/products_screen.dart
@@ -6,32 +6,45 @@ import 'package:fastfood_app/presentation/widgets/filter_tabs.dart';
 import 'package:fastfood_app/presentation/widgets/product_list.dart';
 import 'package:fastfood_app/logic/blocs/category/category_bloc.dart';
 import 'package:fastfood_app/logic/blocs/category/category_event.dart';
+import 'package:fastfood_app/logic/blocs/product/product_bloc.dart';
+import 'package:fastfood_app/logic/blocs/product/product_event.dart';
 import 'package:fastfood_app/data/repositories/category_repository.dart';
+import 'package:fastfood_app/data/repositories/product_repository.dart';
 
 class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF1C2029), // Dark background color
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1C2029),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CategoryBloc(
+            categoryRepository: context.read<CategoryRepository>(),
+          )..add(LoadCategories()),
         ),
-        title: Text('Popular Now', style: TextStyle(color: Colors.white)),
-        centerTitle: true,
-      ),
-      body: BlocProvider(
-        create: (context) => CategoryBloc(categoryRepository: context.read<CategoryRepository>())
-          ..add(LoadCategories()), // Make sure this is imported
-        child: Column(
+        BlocProvider(
+          create: (context) => ProductBloc(
+            productRepository: context.read<ProductRepository>(),
+          )..add(LoadProducts()),
+        ),
+      ],
+      child: Scaffold(
+        backgroundColor: Color(0xFF1C2029), // Dark background color
+        appBar: AppBar(
+          backgroundColor: Color(0xFF1C2029),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          title: Text('Popular Now', style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+        ),
+        body: Column(
           children: [
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: SearchBar(),
+              child: CustomSearchBar(),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
